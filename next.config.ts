4c601d02f2c63a6dbf809eb40c0c35a8b4a3c79f
Manuel Garcia <manuelalejandrogarcia@live.com.ar>
2026-05-21 17:49:18 -0300

    cambios

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
     "192.168.1.*",
     "10.0.0.*",
     "172.16.0.*",
+    "192.168.42.*",  // Android USB tethering
+    "192.168.43.*",  // Android WiFi hotspot
+    "172.20.10.*",   // iPhone hotspot (USB o WiFi)
   ],
 };
 
